732276221d918ebed9e93c2784d897926a9a3200
rianboy18 <pranavr95.pn@gmail.com>
2018-11-05 19:04:58 +0000

    Done

--- q01_feature_engineering/build.py
+++ q01_feature_engineering/build.py
@@ -1,9 +1,24 @@
+# %load q01_feature_engineering/build.py
 import pandas as pd
 import numpy as np
 
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
+df2 = character_predictions
+def q01_feature_engineering(battles,character_predictions):
+    
+    battles['attacker_commander'] = battles['attacker_commander'].fillna(battles['attacker_commander'].mode()[0])
+    battles['defender_1'] = battles['defender_1'].fillna(battles['defender_1'].mode()[0])
+    battles['defender_2'] = battles['defender_2'].fillna(battles['defender_2'].mode()[0])
+    battles['defender_3'] = battles['defender_3'].fillna(0)
+    battles['defender_4'] = battles['defender_4'].fillna(0)
+    battles['defender_count'] = 2
+    battles['attacker_count'] = 4
+    battles['att_comm_count'] = battles['attacker_commander'].str.split(',').str.len()
+    df2['no_of_books'] = df2['book1']+df2['book2']+df2['book3']+df2['book4']+df2['book5']
+    return battles,df2
 
-def q01_feature_engineering():
-    "write your solution here"
+
+
+
 
--- q02_eda_major_death/build.py
+++ q02_eda_major_death/build.py
@@ -1,14 +1,21 @@
+# %load q02_eda_major_death/build.py
 import pandas as pd
 import numpy as np
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_feature_engineering
+import matplotlib.pyplot as plt
 
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
+battles, character_pred = q01_feature_engineering(battles,character_predictions)
+df1 = battles
+df2 = character_predictions
 
-def q02_eda_major_death():
-    "write your solution here"
+def q02_eda_major_death(battles):
+    df3=df1.groupby(['year'])['major_death','major_capture'].sum()
+    df3.plot(x='year', y=['major_death','major_capture'], kind='bar')
+
 
 
 
